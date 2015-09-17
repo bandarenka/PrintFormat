309e9318e7b377568a2f27400682564053688cbe
Admin <admin@Mac-Admin.local>
2015-09-17 12:32:47 +0300

    Initial Commit

---
+++ PrintFormatProject/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PrintFormatProject
+//
+//  Created by Admin on 17.09.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
